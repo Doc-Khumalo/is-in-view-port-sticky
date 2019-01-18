90ec815b9e03c51fa2c0ab5485a2dde5f9c55be4
Leslie Khumalo <leslie.khumalo@icloud.com>
2019-01-18 17:05:07 +0000

    add more scenarios

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 import { topMarkerPosition, isInViewPortTopAddClass } from './topViewPortHelpers/index';
 import { isInViewPortBottomAddClass, bottomMarkerPosition } from './bottomViewPortHelpers/index';
 
-module.exports.topMarkerPosition =  topMarkerPosition;
+module.exports.topMarkerPosition =  topOffset => {
+  topMarkerPosition(topOffset)
+};
+
+module.exports.bottomMarkerPosition =  bottomOffset => {
+  bottomMarkerPosition(bottomOffset)
+};
+
+module.exports.isInViewPortTopAddClass =  (topOffset, scrollOriginMarker, className) => {
+  isInViewPortTopAddClass(topOffset, scrollOriginMarker, className)
+};
+
+module.exports.isInViewPortBottomAddClass =  (bottomOffset, scrollOriginMarker, className) => {
+  isInViewPortBottomAddClass(bottomOffset, scrollOriginMarker, className)
+};
