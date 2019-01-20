df3d3f0a84069f498dff86b48b92efe83cceb889
Leslie Khumalo <leslie.khumalo@icloud.com>
2019-01-20 10:38:10 +0000

    no message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import test from './ViewPortHelpers/isInViewPortTop';
 
-module.exports = StickyTop = topOffset => test(topOffset);
+module.exports = function StickyTop(topOffset) { return test(topOffset)};
 
 /*module.export.topMarkerPosition = topMarkerPosition;
 
