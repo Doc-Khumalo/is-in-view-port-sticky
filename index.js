dd0a64fce2042122b41dc28cc6f5adc471350085
Leslie Khumalo <leslie.khumalo@icloud.com>
2019-01-20 11:46:40 +0000

    no message

--- index.js
+++ index.js
@@ -5,16 +5,3 @@ module.exports.StickyBottom = bottomOffset => custom.isInViewPortBottom(bottomOf
 module.exports.StickyTopAddClass = (topOffset, scrollOriginMarker, className) => custom.isInViewPortTopAddClass(topOffset, scrollOriginMarker, className);
 module.exports.StickyBottomAddClass = (bottomOffset, scrollOriginMarker, className) => custom.isInViewPortBottomAddClass(bottomOffset, scrollOriginMarker, className);
 
-/*module.export.topMarkerPosition = topMarkerPosition;
-
-// module.exports.bottomMarkerPosition = bottomMarkerPosition;
-//
-// module.exports.isInViewPortTopAddClass = isInViewPortTopAddClass;
-//
-// module.exports.isInViewPortBottomAddClass = isInViewPortBottomAddClass;
-
-// exports.testTop = topOffset => isInViewPortTop(topOffset);
-// exports.TestBottom = topOffset => isInViewPortTopBottom;
-
-// if (typeof string !== "string") throw new TypeError("Tiny wants a string!");*/
-
